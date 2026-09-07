0c33b1f624a2525d9b2eb7a38ce753569dce3a2e
Farkas Botond <farkasb@kkszki.hu>
2026-09-07 11:36:05 +0200

    Initial commit.

---
+++ Szemely.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPAlapok
+{
+    internal class Szemely
+    {
+
+        public string Nev { get; set; }
+
+        public Szemely(string nev, int kor)
+        {
+            Nev = nev;
+            Kor = kor;
+        }
+    }
+}
